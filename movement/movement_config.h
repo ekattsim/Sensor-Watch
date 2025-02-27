ee9fa25b1912edd4b02bb55236af1e88f1dd1207
Abhijeet Surakanti <abhijeetsurakanti@gmail.com>
2025-02-26 23:34:50 -0500

    initial test build with tomato, alarm, timer faces

--- movement/movement_config.h
+++ movement/movement_config.h
@@ -29,9 +29,9 @@
 
 const watch_face_t watch_faces[] = {
     simple_clock_face,
-    world_clock_face,
-    sunrise_sunset_face,
-    moon_phase_face,
+    tomato_face,
+    alarm_face,
+    timer_face,
     stopwatch_face,
     preferences_face,
     set_time_face,
